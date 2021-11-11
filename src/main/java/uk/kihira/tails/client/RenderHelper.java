caead53a546981f47b7c5bc98c21e95fcf08baf5
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2021-11-11 02:29:59 -0500

    Replace glScissor stuff with RenderSystem equivalents.

--- src/main/java/uk/kihira/tails/client/RenderHelper.java
+++ src/main/java/uk/kihira/tails/client/RenderHelper.java
@@ -10,7 +10,7 @@
 
 package uk.kihira.tails.client;
 
-import org.lwjgl.opengl.GL11;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
@@ -23,11 +23,10 @@ public class RenderHelper {
 		final double scaleW = (double)mc.getWidth() / mc.getScaledWidth();
 		final double scaleH = (double)mc.getHeight() / mc.getScaledHeight();
 
-		GL11.glEnable(GL11.GL_SCISSOR_TEST);
-		GL11.glScissor((int)Math.floor(x * scaleW), (int)Math.floor(mc.getHeight() - (y + height) * scaleH), (int)Math.floor((x + width) * scaleW) - (int)Math.floor(x * scaleW), (int)Math.floor(mc.getHeight() - y * scaleH) - (int)Math.floor(mc.getHeight() - (y + height) * scaleH)); // Starts from lower left corner (minecraft starts from upper left)
+		RenderSystem.enableScissor((int)Math.floor(x * scaleW), (int)Math.floor(mc.getHeight() - (y + height) * scaleH), (int)Math.floor((x + width) * scaleW) - (int)Math.floor(x * scaleW), (int)Math.floor(mc.getHeight() - y * scaleH) - (int)Math.floor(mc.getHeight() - (y + height) * scaleH)); // Starts from lower left corner (minecraft starts from upper left)
 	}
 
 	public static void endGlScissor() {
-		GL11.glDisable(GL11.GL_SCISSOR_TEST);
+		RenderSystem.disableScissor();
 	}
 }

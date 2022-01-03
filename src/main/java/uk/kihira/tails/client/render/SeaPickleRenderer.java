e5a7017fd953b617e4f7dab061a16369796b0985
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-01-03 18:08:02 -0500

    Remove sea pickle debug code causing model reconstruction every frame.

--- src/main/java/uk/kihira/tails/client/render/SeaPickleRenderer.java
+++ src/main/java/uk/kihira/tails/client/render/SeaPickleRenderer.java
@@ -49,22 +49,10 @@ public class SeaPickleRenderer extends PartRenderer {
 			model.setRotationPoint(0F, 18.2875F, 0F);
 			model.setTextureOffset(0, 1).addBox(-2F, -0.2875F, -2F, 4F, 6F, 4F, 0F, false);
 			model.setTextureOffset(0, 11).addBox(-2F, -0.2375F, -2F, 4F, 0F, 4F, 0F, false);
-
-			final ModelRenderer cube = new ModelRenderer(this);
-			cube.setRotationPoint(0F, -2.2875F, 0F);
-			model.addChild(cube);
-			cube.rotateAngleY = -0.7854F;
-			cube.setTextureOffset(1, 1).addBox(0F, -0.7F, -0.5F, 0F, 3F, 1F, 0F, false);
-			cube.setTextureOffset(0, 2).addBox(-0.5F, -0.7F, 0F, 1F, 3F, 0F, 0F, false);
 		}
 
 		@Override
 		public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, LivingEntity entity, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha, int subtype, float partialTicks) {
-			model = new ModelRenderer(this);
-			model.setRotationPoint(0F, 18.2875F, 0F);
-			model.setTextureOffset(0, 1).addBox(-2F, -0.2875F, -2F, 4F, 6F, 4F, 0F, false);
-			model.setTextureOffset(0, 11).addBox(-2F, -0.2375F, -2F, 4F, 0F, 4F, 0F, false);
-
 			matrixStackIn.push();
 
 			matrixStackIn.translate(0, -2, 0);

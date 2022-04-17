38c2e7b08f0f8bdba8ace5e4d322270900876259
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-04-17 00:13:49 -0400

    Remove old commented code.

--- src/main/java/uk/kihira/tails/client/model/MuzzleModel.java
+++ src/main/java/uk/kihira/tails/client/model/MuzzleModel.java
@@ -23,9 +23,6 @@ import net.minecraft.world.entity.LivingEntity;
  */
 public class MuzzleModel extends PartModel {
 
-	//private final ModelPart stubMuzzle;
-	//private final ModelPart tinyMuzzle;
-
 	private final ModelPart root;
 	private final ModelPart muzzle;
 
@@ -36,12 +33,6 @@ public class MuzzleModel extends PartModel {
 		root = rootDef.bake(32, 32);
 
 		muzzle = root.getChild("muzzle");
-
-		/*stubMuzzle = new ModelPart(this);
-		stubMuzzle.addBox(-2f, -4f, -7f, 4, 4, 3);
-
-		tinyMuzzle = new ModelPart(this);
-		tinyMuzzle.addBox(-2f, -2f, -5f, 4, 2, 1);*/
 	}
 
 	public MuzzleModel(float xOffset, float yOffset, float zOffset, int xSize, int ySize, int zSize) {

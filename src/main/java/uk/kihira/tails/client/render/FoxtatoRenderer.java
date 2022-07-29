a36371da74e6a2b66355ae035b99582e38c7dfc4
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-07-29 16:16:05 -0400

    Disable FoxtatoRenderer for now.

--- src/main/java/uk/kihira/tails/client/render/FoxtatoRenderer.java
+++ src/main/java/uk/kihira/tails/client/render/FoxtatoRenderer.java
@@ -15,23 +15,22 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import uk.kihira.tails.client.FakeEntity;
 import uk.kihira.tails.client.PartRenderRegistry;
 import uk.kihira.tails.common.part.PartInfo;
 import uk.kihira.tails.common.part.PartRegistry;
-import vazkii.botania.api.item.TinyPotatoRenderEvent;
 
 public class FoxtatoRenderer {
 
-	private FoxtatoFakeEntity fakeEntity;
+	/*private FoxtatoFakeEntity fakeEntity;
 	private final PartInfo tailPartInfo = new PartInfo(PartRegistry.FLUFFY_TAIL.getId(), 0, 0, new int[]{-5480951, -6594259, -5197647}, null);
 	private final PartInfo earPartInfo = new PartInfo(PartRegistry.FOX_EARS.getId(), 0, 0, new int[]{-5480951, 0xFF000000, -5197647}, null);
 
 	@SubscribeEvent
-	public void onWorldUnload(WorldEvent.Unload e) {
+	public void onWorldUnload(LevelEvent.Unload e) {
 		if (fakeEntity != null) {
 			fakeEntity.remove(Entity.RemovalReason.DISCARDED);
 			fakeEntity = null;
@@ -69,5 +68,5 @@ public class FoxtatoRenderer {
 		public FoxtatoFakeEntity(Level world) {
 			super(world);
 		}
-	}
+	}*/
 }

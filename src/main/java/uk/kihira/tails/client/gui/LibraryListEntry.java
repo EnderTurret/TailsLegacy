3fe358b511612b8fc883b0ec99b5ebc9d9251978
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-09-18 21:18:15 -0400

    Use the id when showing unknown parts in the library.

--- src/main/java/uk/kihira/tails/client/gui/LibraryListEntry.java
+++ src/main/java/uk/kihira/tails/client/gui/LibraryListEntry.java
@@ -48,7 +48,8 @@ public class LibraryListEntry extends ObjectSelectionList.Entry<LibraryListEntry
 		for (PartType type : PartType.values())
 			if (data.partsData.hasPartInfo(type)) {
 				final ClientPartInfo partInfo = (ClientPartInfo) data.partsData.getPartInfo(type);
-				RenderHelper.drawStringMultiLine(poseStack, fontRenderer, I18n.get(partInfo.getPart().getTranslationKey()),
+				final String trans = partInfo.isInvalid() ? partInfo.getPartId().toString() : I18n.get(partInfo.getPart().getTranslationKey());
+				RenderHelper.drawStringMultiLine(poseStack, fontRenderer, trans,
 						rowLeft + 5, rowTop + 12 + 8 * type.ordinal(), 0xFFFFFF);
 				for (int i = 1; i < 4; i++)
 					GuiComponent.fill(poseStack,

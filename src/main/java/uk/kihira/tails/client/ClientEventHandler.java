00f7a130d17793343c62fc3be5955195340f00df
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-09-19 22:10:27 -0400

    Move layer addition to the client event handler.

--- src/main/java/uk/kihira/tails/client/ClientEventHandler.java
+++ src/main/java/uk/kihira/tails/client/ClientEventHandler.java
@@ -8,14 +8,22 @@
 
 package uk.kihira.tails.client;
 
+import java.util.Map;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.layers.ArrowLayer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -23,7 +31,10 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 import uk.kihira.tails.client.gui.EditorScreen;
 import uk.kihira.tails.client.part.LocalPartManager;
+import uk.kihira.tails.client.render.layer.PartLayer;
+import uk.kihira.tails.client.render.layer.TailsArrowLayer;
 import uk.kihira.tails.common.Tails;
+import uk.kihira.tails.common.part.PartType;
 
 @OnlyIn(Dist.CLIENT)
 public final class ClientEventHandler {
@@ -75,6 +86,37 @@ public final class ClientEventHandler {
 
 					sentPartInfoToServer = true;
 				}
+		}
+	}
+
+	@EventBusSubscriber(modid = Tails.MOD_ID, bus = EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+	static class Mod {
+
+		@SubscribeEvent
+		static void addLayers(EntityRenderersEvent.AddLayers e) {
+			final Minecraft mc = Minecraft.getInstance();
+			final Map<String, EntityRenderer<? extends Player>> skinMap = mc.getEntityRenderDispatcher().getSkinMap();
+
+			// Make a context here because the event doesn't have one even though it's literally three lines away.
+			final EntityRendererProvider.Context ctx = new EntityRendererProvider.Context(mc.getEntityRenderDispatcher(),
+					mc.getItemRenderer(), mc.getBlockRenderer(), mc.getEntityRenderDispatcher().getItemInHandRenderer(),
+					mc.getResourceManager(), mc.getEntityModels(), mc.font);
+
+			for (EntityRenderer<? extends Player> renderer : skinMap.values()) {
+				final PlayerRenderer renderer2 = (PlayerRenderer) renderer;
+				renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().body, PartType.TAIL));
+				renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().body, PartType.WINGS));
+				renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().head, PartType.EARS));
+				renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().head, PartType.MUZZLE));
+
+				for (int i = 0; i < renderer2.layers.size(); i++)
+					// If other mods do this exact same thing, let them take precedence.
+					// If it's just an ArrowLayer mixin, then sucks for them.
+					if (renderer2.layers.get(i).getClass() == ArrowLayer.class) {
+						renderer2.layers.set(i, new TailsArrowLayer<>(ctx, renderer2));
+						break;
+					}
+			}
 		}
 	}
 }
--- src/main/java/uk/kihira/tails/proxy/client/ClientProxy.java
+++ src/main/java/uk/kihira/tails/proxy/client/ClientProxy.java
@@ -8,25 +8,16 @@
 
 package uk.kihira.tails.proxy.client;
 
-import java.util.Map;
-
 import com.google.gson.Gson;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.layers.ArrowLayer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 import uk.kihira.tails.api.IPlayerPartManager;
 import uk.kihira.tails.client.ClientLibraryManager;
@@ -37,11 +28,7 @@ import uk.kihira.tails.client.render.FoxtatoRenderer;
 import uk.kihira.tails.client.render.helper.FakeEntityRenderHelper;
 import uk.kihira.tails.client.render.helper.PlayerRenderHelper;
 import uk.kihira.tails.client.render.helper.RenderHelperManager;
-import uk.kihira.tails.client.render.layer.PartLayer;
-import uk.kihira.tails.client.render.layer.TailsArrowLayer;
 import uk.kihira.tails.common.LibraryManager;
-import uk.kihira.tails.common.Tails;
-import uk.kihira.tails.common.part.PartType;
 import uk.kihira.tails.proxy.IProxy;
 
 /**
@@ -50,7 +37,6 @@ import uk.kihira.tails.proxy.IProxy;
  * please dispose of the class loader immediately and get one that is not a professional client proxy hunter.
  */
 @OnlyIn(Dist.CLIENT)
-@EventBusSubscriber(modid = Tails.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public final class ClientProxy implements IProxy {
 
 	private LibraryManager libraryManager;
@@ -65,33 +51,6 @@ public final class ClientProxy implements IProxy {
 
 		if (ModList.get().isLoaded("botania"))
 			MinecraftForge.EVENT_BUS.register(new FoxtatoRenderer());
-	}
-
-	@SubscribeEvent
-	public static void addLayers(EntityRenderersEvent.AddLayers e) {
-		final Minecraft mc = Minecraft.getInstance();
-		final Map<String, EntityRenderer<? extends Player>> skinMap = mc.getEntityRenderDispatcher().getSkinMap();
-
-		// Make a context here because the event doesn't have one even though it's literally three lines away.
-		final EntityRendererProvider.Context ctx = new EntityRendererProvider.Context(mc.getEntityRenderDispatcher(),
-				mc.getItemRenderer(), mc.getBlockRenderer(), mc.getEntityRenderDispatcher().getItemInHandRenderer(),
-				mc.getResourceManager(), mc.getEntityModels(), mc.font);
-
-		for (EntityRenderer<? extends Player> renderer : skinMap.values()) {
-			final PlayerRenderer renderer2 = (PlayerRenderer) renderer;
-			renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().body, PartType.TAIL));
-			renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().body, PartType.WINGS));
-			renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().head, PartType.EARS));
-			renderer2.addLayer(new PartLayer(renderer2, renderer2.getModel().head, PartType.MUZZLE));
-
-			for (int i = 0; i < renderer2.layers.size(); i++)
-				// If other mods do this exact same thing, let them take precedence.
-				// If it's just an ArrowLayer mixin, then sucks for them.
-				if (renderer2.layers.get(i).getClass() == ArrowLayer.class) {
-					renderer2.layers.set(i, new TailsArrowLayer<>(ctx, renderer2));
-					break;
-				}
-		}
 	}
 
 	@Override

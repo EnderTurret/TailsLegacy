7be293901c8c545e6d4d128836337ce4376a9d53
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2023-06-14 15:47:30 -0400

    Adjust position of horns slightly.

--- src/main/java/uk/kihira/tails/client/model/ears/HornsModel.java
+++ src/main/java/uk/kihira/tails/client/model/ears/HornsModel.java
@@ -34,24 +34,19 @@ final class HornsModel extends PartModel {
 
 		final PartDefinition right = rootDef.addOrReplaceChild("right", CubeListBuilder.create(), PartPose.offset(-2.1691F, -6.591F, -3.5F));
 
-		right.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 8).addBox(-0.9F, -2.8536F, -1F, 1F, 3F, 1F), PartPose.offsetAndRotation(-0.7497F, -1.9489F, -0.5176F, 0.0873F, -0.0348F, 0.2413F));
-		right.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(10, 5).addBox(-0.9042F, -0.9292F, -1.0529F, 1F, 2F, 1F), PartPose.offsetAndRotation(-0.1497F, -2.0666F, -0.2929F, 0.2618F, 0.0537F, -0.4842F));
-		right.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(8, 0).addBox(-1F, -0.5F, -1F, 1F, 2F, 1F), PartPose.offsetAndRotation(-0.7259F, -1.8554F, -0.4223F, 0.2618F, 0F, -0.1745F));
-		right.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(0, 4).addBox(-1F, -1F, -1F, 2F, 2F, 2F), PartPose.offsetAndRotation(0F, 0F, 0F, 0F, 0F, 0.7854F));
+		right.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 8).addBox(-0.9F, -2.8536F, -1F, 1F, 3F, 1F), PartPose.offsetAndRotation(-0.7497F, -2.0489F, -0.6176F, 0.0873F, -0.0348F, 0.2413F));
+		right.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(10, 5).addBox(-0.9042F, -0.9292F, -1.0529F, 1F, 2F, 1F), PartPose.offsetAndRotation(-0.1497F, -2.1666F, -0.3929F, 0.2618F, 0.0537F, -0.4842F));
+		right.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(8, 0).addBox(-1F, -0.5F, -1F, 1F, 2F, 1F), PartPose.offsetAndRotation(-0.7259F, -1.9554F, -0.5223F, 0.2618F, 0F, -0.1745F));
+		right.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(0, 4).addBox(-1F, -1F, -1F, 2F, 2F, 2F), PartPose.offsetAndRotation(0F, -0.1F, -0.1F, 0F, 0F, 0.7854F));
 
 		final PartDefinition left = rootDef.addOrReplaceChild("left", CubeListBuilder.create(), PartPose.offset(2.1691F, -6.591F, -3.5F));
 
-		left.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 8).mirror().addBox(-0.1F, -2.8536F, -1F, 1F, 3F, 1F).mirror(false), PartPose.offsetAndRotation(0.7497F, -1.9489F, -0.5176F, 0.0873F, 0.0348F, -0.2413F));
-		left.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(10, 5).mirror().addBox(-0.0958F, -0.9292F, -1.0529F, 1F, 2F, 1F).mirror(false), PartPose.offsetAndRotation(0.1497F, -2.0666F, -0.2929F, 0.2618F, -0.0537F, 0.4842F));
-		left.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(8, 0).mirror().addBox(0F, -0.5F, -1F, 1F, 2F, 1F).mirror(false), PartPose.offsetAndRotation(0.7259F, -1.8554F, -0.4223F, 0.2618F, 0F, 0.1745F));
-		left.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(0, 4).mirror().addBox(-1F, -1F, -1F, 2F, 2F, 2F).mirror(false), PartPose.offsetAndRotation(0F, 0F, 0F, 0F, 0F, -0.7854F));
+		left.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 8).mirror().addBox(-0.1F, -2.8536F, -1F, 1F, 3F, 1F).mirror(false), PartPose.offsetAndRotation(0.7497F, -2.0489F, -0.6176F, 0.0873F, 0.0348F, -0.2413F));
+		left.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(10, 5).mirror().addBox(-0.0958F, -0.9292F, -1.0529F, 1F, 2F, 1F).mirror(false), PartPose.offsetAndRotation(0.1497F, -2.1666F, -0.3929F, 0.2618F, -0.0537F, 0.4842F));
+		left.addOrReplaceChild("cube_r7", CubeListBuilder.create().texOffs(8, 0).mirror().addBox(0F, -0.5F, -1F, 1F, 2F, 1F).mirror(false), PartPose.offsetAndRotation(0.7259F, -1.9554F, -0.5223F, 0.2618F, 0F, 0.1745F));
+		left.addOrReplaceChild("cube_r8", CubeListBuilder.create().texOffs(0, 4).mirror().addBox(-1F, -1F, -1F, 2F, 2F, 2F).mirror(false), PartPose.offsetAndRotation(0F, -0.1F, -0.1F, 0F, 0F, -0.7854F));
 
 		root = rootDef.bake(16, 16);
-	}
-
-	@Override
-	public void setupPartPreviewAnim(RenderContext ctx, PartRenderer renderer) {
-		
 	}
 
 	@Override

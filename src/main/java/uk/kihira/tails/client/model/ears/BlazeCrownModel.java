0e27de983e0719721b8fe9b4f030543a7f262cfd
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2024-11-19 02:40:08 -0500

    Get some stragglers.

--- src/main/java/uk/kihira/tails/client/model/ears/BlazeCrownModel.java
+++ src/main/java/uk/kihira/tails/client/model/ears/BlazeCrownModel.java
@@ -28,46 +28,10 @@ import uk.kihira.tails.client.render.part.PartRenderer;
  */
 final class BlazeCrownModel extends PartModel {
 
-	private final ModelPart root;
-	private final ModelPart crown;
-	private final ModelPart rods;
-
-	public BlazeCrownModel() {
-		final PartDefinition rootDef = new MeshDefinition().getRoot();
-
-		final PartDefinition crown = rootDef.addOrReplaceChild("crown", CubeListBuilder.create()
-				.texOffs(0, 9).addBox(-4.5F, -7F, -4F, 1F, 1F, 8F, new CubeDeformation(0.01F))
-				.texOffs(0, 0).addBox(3.5F, -7F, -4F, 1F, 1F, 8F, new CubeDeformation(0.01F))
-				.texOffs(10, 2).addBox(-4.6F, -7.1F, -4.5F, 9F, 1F, 1F, new CubeDeformation(0.01F))
-				.texOffs(10, 0).addBox(-4.4F, -7.1F, 3.5F, 9F, 1F, 1F, new CubeDeformation(0.01F)),
-				PartPose.ZERO);
-
-		crown.addOrReplaceChild("rods", CubeListBuilder.create()
-				.texOffs(0, 18).addBox(-2F, -9.5F, -5F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(0, 0).addBox(-2F, -8.5F, 4F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(14, 9).addBox(-5F, -7.5F, -1F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(4, 9).addBox(4F, -7.5F, -1F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(10, 9).addBox(-5F, -8.5F, 2F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(0, 9).addBox(4F, -8.5F, 2F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(17, 17).addBox(1F, -8.5F, -5F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(4, 0).addBox(1F, -9.5F, 4F, 1F, 4F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(8, 18).addBox(3F, -8F, -5F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(10, 4).addBox(3F, -8F, 4F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(18, 4).addBox(-4F, -8F, -5F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(14, 4).addBox(-4F, -8F, 4F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(4, 18).addBox(-5F, -8F, -3F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F))
-				.texOffs(17, 13).addBox(4F, -8F, -3F, 1F, 3F, 1F, new CubeDeformation(0F, 0.01F, 0F)),
-				PartPose.ZERO);
-
-		root = ModelSerializer.bake(rootDef, 32, 32, "ears/blaze_crown");
-
-		this.crown = root.getChild("crown");
-		rods = this.crown.getChild("rods");
-	}
-
 	@Override
 	public void render(RenderContext ctx) {
-		ctx.render(crown);
-		ctx.render(rods, LightTexture.FULL_BRIGHT, ctx.packedOverlay());
+		final ModelPart model = ctx.getModel();
+		ctx.render(model);
+		ctx.render(model.getChild("crown").getChild("rods"), LightTexture.FULL_BRIGHT, ctx.packedOverlay());
 	}
 }
--- src/main/java/uk/kihira/tails/client/model/ears/FoxEarsModel.java
+++ src/main/java/uk/kihira/tails/client/model/ears/FoxEarsModel.java
@@ -24,44 +24,12 @@ import uk.kihira.tails.client.render.RenderContext;
  */
 final class FoxEarsModel extends PartModel {
 
-	private final ModelPart root;
-	private final ModelPart leftEar;
-	private final ModelPart rightEar;
-
-	public FoxEarsModel() {
-		final PartDefinition rootDef = new MeshDefinition().getRoot();
-
-		rootDef.addOrReplaceChild("leftEar", CubeListBuilder.create()
-				.mirror()
-				.texOffs(0, 16).addBox(4, -11, 1, 1, 1, 1)
-				.texOffs(4, 16).addBox(3, -10, 1, 2, 2, 1)
-				.texOffs(0, 0).addBox(2, -10, 1, 1, 3, 1)
-				.texOffs(4, 0).addBox(3, -11, 1, 1, 1, 1)
-				.texOffs(4, 4).addBox(4, -12, 1, 1, 1, 1)
-				.texOffs(0, 8).addBox(5, -11, 1, 1, 3, 1)
-				.texOffs(10, 14).addBox(3, -8, 1, 2, 1, 1)
-				.texOffs(4, 8).addBox(4, -11, 2, 1, 3, 1)
-				.texOffs(8, 0).addBox(3, -10, 2, 1, 2, 1), PartPose.ZERO);
-
-		rootDef.addOrReplaceChild("rightEar", CubeListBuilder.create()
-				.texOffs(0, 19).addBox(-5, -11, 1, 1, 1, 1)
-				.texOffs(4, 19).addBox(-5, -10, 1, 2, 2, 1)
-				.texOffs(0, 4).addBox(-3, -10, 1, 1, 3, 1)
-				.texOffs(4, 2).addBox(-4, -11, 1, 1, 1, 1)
-				.texOffs(4, 6).addBox(-5, -12, 1, 1, 1, 1)
-				.texOffs(0, 12).addBox(-6, -11, 1, 1, 3, 1)
-				.texOffs(10, 12).addBox(-5, -8, 1, 2, 1, 1)
-				.texOffs(4, 12).addBox(-5, -11, 2, 1, 3, 1)
-				.texOffs(8, 3).addBox(-4, -10, 2, 1, 2, 1), PartPose.ZERO);
-
-		root = ModelSerializer.bake(rootDef, 16, 32, "ears/fox_ears");
-
-		leftEar = root.getChild("leftEar");
-		rightEar = root.getChild("rightEar");
-	}
-
 	@Override
 	public void render(RenderContext ctx) {
+		final ModelPart model = ctx.getModel();
+		final ModelPart leftEar = model.getChild("leftEar");
+		final ModelPart rightEar = model.getChild("rightEar");
+
 		ctx.poseStack().pushPose();
 
 		ctx.poseStack().translate(0f, 0f, -0.0625f);

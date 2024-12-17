b0b86b2eb192aaa11a8f8163c7be0f247f13f748
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2024-12-16 20:15:57 -0500

    Attempt to dodge an NPE.

--- src/main/java/uk/kihira/tails/client/gui/panel/PartsPanel.java
+++ src/main/java/uk/kihira/tails/client/gui/panel/PartsPanel.java
@@ -116,7 +116,7 @@ public final class PartsPanel extends Panel<EditorScreen> {
 			final String subId = subType != null ? subType.id() : oldInfo.getSubTypeId();
 			final String textureId = texture != null ? texture.id() : oldInfo.getTextureId();
 			final ServerPartInfo spi = new ServerPartInfo(entry.partInfo.getPart().getId(), subId, textureId, oldInfo.getTints().clone());
-			partInfo = new ClientPartInfo(spi, PartRegistry.reference(entry.partInfo.getPart().getId()), subType.id(), texture.id());
+			partInfo = new ClientPartInfo(spi, PartRegistry.reference(entry.partInfo.getPart().getId()), subId, textureId);
 		}
 
 		// Breaks immutability, but it's probably fine, right?

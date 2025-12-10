4898ad613ea669f36ed92b85ac0bf96902ce718e
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2025-12-10 02:04:09 -0500

    Add coremod.

---
+++ forge-1.12.2/src/main/java/uk/kihira/tails/forge/asm/TailsPlugin.java
@@ -0,0 +1,35 @@
+package uk.kihira.tails.forge.asm;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+@IFMLLoadingPlugin.Name("Tails")
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.SortingIndex(1000)
+public class TailsPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+	@Override
+	public String[] getASMTransformerClass() { return null; }
+
+	@Override
+	public String getModContainerClass() { return null; }
+
+	@Override
+	public String getSetupClass() { return null; }
+
+	@Override
+	public void injectData(Map<String, Object> data) {}
+
+	@Override
+	public String getAccessTransformerClass() { return null; }
+
+	@Override
+	public List<String> getMixinConfigs() {
+		return Collections.singletonList("mixins.tails.json");
+	}
+}

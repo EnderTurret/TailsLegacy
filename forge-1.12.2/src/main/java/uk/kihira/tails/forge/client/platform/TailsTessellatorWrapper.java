b9dd9bb45684ea48a9e98f7e5c7e9368d7b71082
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2025-12-10 20:41:02 -0500

    Omit the normals for the wings in 1.12.
    This doesn't appear to change anything, but it's closer to the original.

--- forge-1.12.2/src/main/java/uk/kihira/tails/forge/client/platform/TailsTessellatorWrapper.java
+++ forge-1.12.2/src/main/java/uk/kihira/tails/forge/client/platform/TailsTessellatorWrapper.java
@@ -59,7 +59,7 @@ public final class TailsTessellatorWrapper implements TailsBufferSource, TailsBu
 	public void t$submitCustomGeometry(TailsPoseStack poseStack, BiConsumer<Entry, TailsVertexConsumer> renderer) {
 		if (renderingTransparent)
 			GlStateManager.enableBlendProfile(GlStateManager.Profile.TRANSPARENT_MODEL);
-		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
 
 		renderer.accept(poseStack.t$lastEntry(), this);
 
@@ -122,7 +122,7 @@ public final class TailsTessellatorWrapper implements TailsBufferSource, TailsBu
 
 	@Override
 	public TailsVertexConsumer t$normal(Entry pose, float x, float y, float z) {
-		buffer.normal(x, y, z);
+		//buffer.normal(x, y, z);
 		return this;
 	}
 

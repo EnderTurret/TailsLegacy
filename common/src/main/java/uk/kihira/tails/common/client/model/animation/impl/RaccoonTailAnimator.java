c4f04456b5bb6d9aae6df0886b94ebd519094693
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2025-12-29 18:19:31 -0500

    Add license headers to animation code.

--- common/src/main/java/uk/kihira/tails/common/client/model/animation/impl/RaccoonTailAnimator.java
+++ common/src/main/java/uk/kihira/tails/common/client/model/animation/impl/RaccoonTailAnimator.java
@@ -1,3 +1,12 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 Zoe Lee (Kihira)
+ * Copyright (c) 2020-2025 EnderTurret
+ *
+ * See LICENSE for full License
+ */
+
 package uk.kihira.tails.common.client.model.animation.impl;
 
 import com.google.gson.JsonObject;

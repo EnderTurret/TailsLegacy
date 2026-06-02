ca890601f4962b5d3cedcd093aadaeb3f3026005
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2026-06-02 12:10:21 -0400

    Improve test logging.

--- common/src/test/java/test/platform/SimpleTailsPlatformImpl.java
+++ common/src/test/java/test/platform/SimpleTailsPlatformImpl.java
@@ -1,5 +1,6 @@
 package test.platform;
 
+import java.io.PrintStream;
 import java.util.UUID;
 
 import net.enderturret.tailslegacy.common.TailsPlatform;
@@ -11,34 +12,50 @@ public final class SimpleTailsPlatformImpl implements TailsPlatform {
 	// The Log4J at home:
 	@Override
 	public void logDebug(String msg, Object... args) {
-		for (int i = 0; i < args.length; i++) args[i] = String.valueOf(args[i]);
-		System.out.printf("[00xxx0000 00:00:00.000] [main/DEBUG] [Tails Legacy/]: " + msg.replace("{}", "%s") + "\n", args);
+		log(System.out, "DEBUG", msg, args);
 	}
 
 	@Override
 	public void logInfo(String msg) {
-		System.out.println("[00xxx0000 00:00:00.000] [main/INFO] [Tails Legacy/]: " + msg);
+		log(System.out, "INFO", msg);
 	}
 
 	@Override
 	public void logInfo(String msg, Object arg1) {
-		System.out.printf("[00xxx0000 00:00:00.000] [main/INFO] [Tails Legacy/]: " + msg.replace("{}", "%s") + "\n", String.valueOf(arg1));
+		log(System.out, "INFO", msg, arg1);
 	}
 
 	@Override
 	public void logInfo(String msg, Object arg1, Object arg2) {
-		System.out.printf("[00xxx0000 00:00:00.000] [main/INFO] [Tails Legacy/]: " + msg.replace("{}", "%s") + "\n", String.valueOf(arg1), String.valueOf(arg2));
+		log(System.out, "INFO", msg, arg1, arg2);
 	}
 
 	@Override
 	public void logInfo(String msg, Object arg1, Object arg2, Object arg3) {
-		System.out.printf("[00xxx0000 00:00:00.000] [main/INFO] [Tails Legacy/]: " + msg.replace("{}", "%s") + "\n", String.valueOf(arg1), String.valueOf(arg2), String.valueOf(arg3));
+		log(System.out, "INFO", msg, arg1, arg2, arg3);
 	}
 
 	@Override
 	public void logError(String msg, Object... args) {
-		for (int i = 0; i < args.length; i++) args[i] = String.valueOf(args[i]);
-		System.err.printf("[00xxx0000 00:00:00.000] [main/ERROR] [Tails Legacy/]: " + msg.replace("{}", "%s") + "\n", args);
+		log(System.err, "ERROR", msg, args);
+	}
+
+	private static void log(PrintStream out, String level, String message, Object... args) {
+		message = message.replace("{}", "%s");
+
+		Throwable t = null;
+		if (args.length > 0) {
+			if (args[args.length - 1] instanceof Throwable)
+				t = (Throwable) args[args.length - 1];
+
+			for (int i = 0; i < args.length; i++) args[i] = String.valueOf(args[i]);
+		}
+
+		out.printf("[00xxx0000 00:00:00.000] [main/" + level + "] [Tails Legacy/]: " + message + "\n", args);
+
+		if (t != null) t.printStackTrace(out);
+
+		out.flush();
 	}
 
 	@Override
